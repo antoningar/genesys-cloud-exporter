81942a0dccd9626f5f005cedeeb06b8e2bd5f304
antoningar <antoningaranto@protonmail.com>
2025-04-22 21:58:21 +0100

    fix(campaign): active campaign

--- terraform/resource_outbound_campaign.tf
+++ terraform/resource_outbound_campaign.tf
@@ -4,7 +4,7 @@ resource "genesyscloud_outbound_campaign" "exoporter_campaign" {
   caller_name     = ""
   caller_address  = ""
   contact_list_id = genesyscloud_outbound_contact_list.contact-list.id
-  campaign_status = "off"
+  campaign_status = "on"
   queue_id        = genesyscloud_routing_queue.tmp_exporter_queue.id
   script_id       = data.genesyscloud_script.default_script.id
   phone_columns {
